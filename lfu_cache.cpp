6681b8bd6ed3f41a688fcce6ed35ef45aa2751d6
Владислав Игоревич Лазарь <vlazar@ispras.ru>
2024-10-02 00:31:39 +0300

    The cache has been written and tested. Requires optimization on big data

---
+++ lfu_cache.cpp
@@ -0,0 +1,29 @@
+#include "lfu_cache.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int slow_get_page(int key) {
+    return key;
+}
+
+int main() {
+    size_t cacheSize, pagesNum;
+    std::vector<int> cacheBuf;
+    std::ifstream inputFile("data.txt");
+
+    inputFile >> cacheSize >> pagesNum;
+    LFUCache<int, int> cache(cacheSize);
+
+    for(int i = 0, data = 0; i < pagesNum; ++i) {
+        inputFile >> data;
+        cacheBuf.push_back(data);
+    }
+
+    size_t hits = 0;
+    for(int i = 0; i < pagesNum; i++) {
+        hits += cache.lookup_update(cacheBuf[i], slow_get_page);
+    }
+
+    std::cout << "HITS: " << hits << std::endl;
+}
